0387eda6b6098042089d4578f24ff5b15543487f
ZacMarshall1 <juliokretschmerb@gmail.com>
2025-04-22 18:40:06 -0300

    desafio 4 completo com data atual

--- d004/util/ieneParaReal.php
+++ d004/util/ieneParaReal.php
@@ -2,13 +2,13 @@
 
     function cotacao_iene(){
 
-        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=\'JPY\'&@dataInicial=\'04-09-2025\'&@dataFinalCotacao=\'04-16-2025\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao';
-
+        $inicio = date("m/d/Y", strtotime("-7 days"));
+        $fim = date("m/d/Y");
+        $url = 'https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaPeriodo(moeda=@moeda,dataInicial=@dataInicial,dataFinalCotacao=@dataFinalCotacao)?@moeda=\'JPY\'&@dataInicial=\''. $inicio.'\'&@dataFinalCotacao=\''. $fim .'\'&$top=1&$orderby=dataHoraCotacao%20desc&$format=json&$select=cotacaoCompra,cotacaoVenda,dataHoraCotacao';
         $dados = json_decode(file_get_contents($url), true);
-
         $cotacao = $dados["value"][0]["cotacaoCompra"];
-
         return $cotacao;
+        
     }
 
     function iene_para_real(){
